50384093df0e3a93e4e9f1bbd592d4a6054de541
jbt <doronsaroudi@gmail.com>
2026-09-08 11:21:26 +0300

    add board withmine and flag

--- game_field.py
+++ game_field.py
@@ -2,34 +2,61 @@ import consets
 import random
 
 
-
 def generate_random_dungeon():
     board = []
     for row in range(consets.BOARD_ROWS):
         row = []
         for col in range(consets.BOARD_COLS):
-            rand_val = random.random()
-            if rand_val < 0.3:
-                row.append("mine")
-            else:
-                row.append("empty")
+            row.append("empty")
 
         board.append(row)
 
-    board = generate_flag(board)
+    return generate_flag_and_mines(board)
+
+
+def generate_mines(board, flag_row_index, flag_col_index):
+    mine_count = consets.MINES_COUNT
+    mine_row = consets.MINE_ROWS
+    mine_col = consets.MINE_COLS
+    for i in range(mine_count):
+        row = random.randint(0, consets.BOARD_ROWS - 1)
+        col = random.randint(0, consets.BOARD_COLS - 1)
+
+        if check_available_mines(board, row, col,mine_row,mine_col):
+            for k in range(mine_row):
+                for l in range(mine_col):
+                    board[row + k][col + l] = "mine"
+
+
+        else:
+            i -= 1
 
     return board
 
+def check_available_mines(board, row, col,mine_row,mine_col):
+    for i in range(mine_row):
+        for j in range(mine_col):
+            try:
+                if board[row + i][col + j] == "flag":
+                    return False
+            except IndexError:
+                return False
 
-def generate_flag(board):
+    return True
+
+
+
+def generate_flag_and_mines(board):
     flag_row_index = consets.flag_row
     flag_col_index = consets.flag_col
 
     for i in range(consets.FLAG_ROWS):
         for j in range(consets.FLAG_COLS):
-            board[flag_row_index+i][flag_col_index+j] = "flag"
+            board[flag_row_index + i][flag_col_index + j] = "flag"
 
+    board = generate_mines(board, flag_row_index, flag_col_index)
     return board
+
 
 
 board = generate_random_dungeon()
